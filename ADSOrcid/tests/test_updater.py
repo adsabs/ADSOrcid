116d54d5a26fec5aaefb2db58b05ed601c779fe2
Kelly Lockhart <2926089+kelockhart@users.noreply.github.com>
2018-02-13 16:30:49 -0500

    Added tests for updater and reports to bring test coverage over 80 percent

--- ADSOrcid/tests/test_updater.py
+++ ADSOrcid/tests/test_updater.py
@@ -3,11 +3,30 @@
 
 
 import unittest
+import os
 from mock import patch
-from ADSOrcid import updater, names
+from ADSOrcid import updater, names, app
+from ADSOrcid.models import Base
 
 class Test(unittest.TestCase):
-    
+
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        proj_home = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
+        self.app = app.ADSOrcidCelery('test', local_config= \
+            {
+                'SQLALCHEMY_URL': 'sqlite:///',
+                'SQLALCHEMY_ECHO': False,
+                'PROJ_HOME': proj_home,
+                'TEST_DIR': os.path.join(proj_home, 'ADSOrcid/tests'),
+            })
+        Base.metadata.bind = self.app._session.get_bind()
+        Base.metadata.create_all()
+
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+        Base.metadata.drop_all()
+        self.app.close_app()
     
     def test_update_record(self):
         """
@@ -89,6 +108,43 @@ class Test(unittest.TestCase):
             ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '0000-0003-2686-9241', '-'])
         
         self.assertEqual(14, len(doc['claims']['verified']))
+
+        doc_lev = {
+            'bibcode': '2015ApJ...799..123B',
+            'authors': [
+              "Barrière, Nicolas M.",
+              "Krivonos, Roman",
+              "Tomsick, John A.",
+              "Bachetti, Matteo",
+              "Boggs, Steven E.",
+              "Chakrabarty, Deepto",
+              "Christensen, Finn E.",
+              "Craig, William W.",
+              "Hailey, Charles J.",
+              "Harrison, Fiona A.",
+              "Hong, Jaesub",
+              "Mori, Kaya",
+              "Stern, Daniel",
+              "Zhang, William W."
+            ],
+            'claims': {}
+        }
+        r_lev = updater.update_record(
+            doc_lev,
+            {
+                'bibcode': '2015ApJ...799..123B',
+                'orcidid': '0000-0001-2345-6789',
+                'account_id': '1',
+                'orcid_name': [u'Zhang, Will'],
+                'author': [u'Zhang, Will'],
+                'author_norm': [u'Zhang, Will'],
+                'name': u'Zhang, Will'
+            },
+            0.75
+        )
+        self.assertEqual(r_lev, ('verified', 13))
+        self.assertEqual(doc_lev['claims']['verified'],
+                         ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '0000-0001-2345-6789'])
 
     def test_exact_match(self):
         """
@@ -187,6 +243,54 @@ class Test(unittest.TestCase):
               ["Frey, Katie"]);
         self.assertEqual(res, 2)
 
-            
+    def test_reindex_all_claims(self):
+        """
+        Given an ORCID ID and a starting date, we should be able to reindex all claims after that date.
+
+        :return: None
+        """
+        with patch.object(self.app, 'retrieve_orcid') as retrieve_orcid:
+
+            retrieve_orcid.return_value = {'status': None,
+                                           'name': u'Payne, Cecilia',
+                                           'facts': {u'author': [u'Payne, Cecilia'],
+                                                     u'orcid_name': [u'Payne, Cecilia'],
+                                                     u'name': u'Payne, Cecilia'},
+                                           'orcidid': u'0000-0001-0002-0003',
+                                           'id': 1,
+                                           'account_id': None,
+                                           'updated': '2017-01-01T12:00:00.000000Z'
+                                           }
+
+            cdate = '2018-01-01T12:00:00.000000Z'
+            sdate = '2018-01-01T01:00:00.000000Z'
+
+            self.app.insert_claims([self.app.create_claim(bibcode='2018Test....123...A',
+                                                          orcidid='0000-0001-0002-0003',
+                                                          provenance='Test',
+                                                          status='claimed',
+                                                          date=cdate
+                                                          ),
+                                    self.app.create_claim(bibcode='2018Test....123...B',
+                                                          orcidid='0000-0001-0002-0003',
+                                                          provenance='Test',
+                                                          status='removed',
+                                                          date=cdate
+                                                          )])
+
+            self.app.record_claims('2018Test....123...A',
+                                   {'verified': ['0000-0001-0002-0003', '-', '-'],
+                                    'unverified': ['-', '-', '-']})
+            self.app.record_claims('2018Test....123...B',
+                                   {'verified': ['0000-0001-0002-0003', '-', '-'],
+                                    'unverified': ['-', '-', '-']})
+
+            recs = updater.reindex_all_claims(self.app, orcidid='0000-0001-0002-0003', since=sdate)
+
+            self.assertEqual(len(recs),2)
+            self.assertEqual(sorted(recs)[0],'2018Test....123...A')
+            self.assertEqual(sorted(recs)[1],'2018Test....123...B')
+
+
 if __name__ == '__main__':
     unittest.main()            
