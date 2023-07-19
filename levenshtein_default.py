4bb7ce89c2d79a251830b0d32a5bbc3320fd9fde
femalves <fernandamalves@ufrn.edu.br>
2023-07-19 11:16:29 -0400

    fixing odd import error

--- levenshtein_default.py
+++ levenshtein_default.py
@@ -6,40 +6,47 @@ import Levenshtein
 import sys
 import os
 import matplotlib
-matplotlib.use('TkAgg')
-from matplotlib import pyplot as py
+
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as py
 
 app = tasks.app
-logger = setup_logging('levenshtein_test')
+logger = setup_logging("levenshtein_test")
+
 
 def extract_names(author_dict):
     authors = []
-    for f in ('author', 'orcid_name', 'author_norm', 'short_name'):
+    for f in ("author", "orcid_name", "author_norm", "short_name"):
         if f in author_dict and author_dict[f]:
             for name in author_dict[f]:
                 authors.append(name)
 
     return authors
 
-def query_solr(endpoint, query, start=0, rows=200, sort="date desc", fl='bibcode'):
-    d = {'q': query,
-         'sort': sort,
-         'start': start,
-         'rows': rows,
-         'wt': 'json',
-         'indent': 'true'
-         }
+
+def query_solr(endpoint, query, start=0, rows=200, sort="date desc", fl="bibcode"):
+    d = {
+        "q": query,
+        "sort": sort,
+        "start": start,
+        "rows": rows,
+        "wt": "json",
+        "indent": "true",
+    }
     if fl:
-        d['fl'] = fl
+        d["fl"] = fl
 
     response = app.client.get(endpoint, params=d)
     if response.status_code == 200:
         results = response.json()
         return results
-    logger.warn('For query {0}, there was a network problem: {1}\n'.format(query,response))
+    logger.warn(
+        "For query {0}, there was a network problem: {1}\n".format(query, response)
+    )
     return None
 
-def get_max_lev(orcid_field='orcid_user'):
+
+def get_max_lev(orcid_field="orcid_user"):
     """
     For every author with an ORCID ID in the authors table in postgres, checks SOLR for any accepted claims (in
     orcid_pub, orcid_other, or orcid_user) that they have. Calculates the maximum Levenshtein distance ratio
@@ -50,48 +57,61 @@ def get_max_lev(orcid_field='orcid_user'):
     """
 
     max_lev_dict = {}
-    logger.info('Starting max lev function for {}'.format(orcid_field))
+    logger.info("Starting max lev function for {}".format(orcid_field))
     num_rec = 0
     with app.session_scope() as session:
         # get list of ORCID IDs to check from the authors table in postgres. This will get all ORCID IDs for
         # orcid_user and orcid_other, but will miss some for orcid_pub
         for r in session.query(AuthorInfo).order_by(AuthorInfo.id.asc()).all():
             if num_rec % 100 == 0:
-                logger.info('Querying record {} for {}'.format(num_rec,orcid_field))
+                logger.info("Querying record {} for {}".format(num_rec, orcid_field))
             rec = r.toJSON()
-            orcidid = rec['orcidid']
+            orcidid = rec["orcidid"]
             # use all name variants listed in postgres for a given author
-            authors = extract_names(rec['facts'])
-            if orcid_field == 'orcid_pub':
-                response = query_solr(config['SOLR_URL_OLD'], 'orcid_pub:"' + orcidid + '"', sort="bibcode desc", fl='*')
-            elif orcid_field == 'orcid_other':
-                response = query_solr(config['SOLR_URL_OLD'], 'orcid_other:"' + orcidid + '"', sort="bibcode desc",
-                                      fl='*')
+            authors = extract_names(rec["facts"])
+            if orcid_field == "orcid_pub":
+                response = query_solr(
+                    config["SOLR_URL_OLD"],
+                    'orcid_pub:"' + orcidid + '"',
+                    sort="bibcode desc",
+                    fl="*",
+                )
+            elif orcid_field == "orcid_other":
+                response = query_solr(
+                    config["SOLR_URL_OLD"],
+                    'orcid_other:"' + orcidid + '"',
+                    sort="bibcode desc",
+                    fl="*",
+                )
             else:
-                response = query_solr(config['SOLR_URL_OLD'], 'orcid_user:"' + orcidid + '"', sort="bibcode desc",
-                                      fl='*')
-            if response['response']['docs'] == []:
+                response = query_solr(
+                    config["SOLR_URL_OLD"],
+                    'orcid_user:"' + orcidid + '"',
+                    sort="bibcode desc",
+                    fl="*",
+                )
+            if response["response"]["docs"] == []:
                 num_rec += 1
                 continue
             else:
-                data = response['response']['docs']
+                data = response["response"]["docs"]
                 lev_all = []
                 for record in data:
                     idx = np.where(np.array(record[orcid_field]) == orcidid)
-                    rec_author = record['author'][idx[0][0]]
+                    rec_author = record["author"][idx[0][0]]
                     tmplev = []
                     for var in authors:
                         # make sure the two comparison strings are the same type
                         try:
-                            rec_author = unicode(rec_author,'utf-8')
+                            rec_author = unicode(rec_author, "utf-8")
                         except TypeError:
                             rec_author = rec_author
                         try:
-                            var = unicode(var,'utf-8')
+                            var = unicode(var, "utf-8")
                         except TypeError:
                             var = var
                         # check the Levenshtein ratio for all variant names
-                        tmplev.append(Levenshtein.ratio(rec_author,var))
+                        tmplev.append(Levenshtein.ratio(rec_author, var))
                     # keep only the best one for each record
                     lev_all.append(max(tmplev))
             max_lev_dict[orcidid] = lev_all
@@ -103,6 +123,7 @@ def get_max_lev(orcid_field='orcid_user'):
 
     return max_lev
 
+
 def get_mismatch_lev(save_path=os.getcwd()):
     """
     Gets Levenshtein ratios for rejected claims from log files.
@@ -113,19 +134,20 @@ def get_mismatch_lev(save_path=os.getcwd()):
     :return bad_lev: a list of the Levenshtein ratios of the author names from the rejected claims
     """
 
-    mismatch_file = save_path + '/logs_mismatch.txt'
+    mismatch_file = save_path + "/logs_mismatch.txt"
     with open(mismatch_file) as f:
         lines = f.readlines()
 
     bad_lev = []
-    search_string = 'No match found: the closest is: ('
+    search_string = "No match found: the closest is: ("
     for line in lines:
-        beg = line.find(search_string)+len(search_string)
-        bad_lev.append(float(line[beg:line.find(',',beg)]))
+        beg = line.find(search_string) + len(search_string)
+        bad_lev.append(float(line[beg : line.find(",", beg)]))
 
     return bad_lev
 
-def plot_hist(orcid_field='orcid_user',save_path=os.getcwd()):
+
+def plot_hist(orcid_field="orcid_user", save_path=os.getcwd()):
     """
     Plots histograms of the Levenshtein ratio for every claim, accepted (into orcid_pub, orcid_user,
     orcid_other) or rejected
@@ -136,37 +158,40 @@ def plot_hist(orcid_field='orcid_user',save_path=os.getcwd()):
     :param save_path: path to save figure image files
     :return: None
     """
-    if orcid_field == 'mismatch':
+    if orcid_field == "mismatch":
         lev = get_mismatch_lev(save_path=save_path)
-        plcol = 'red'
+        plcol = "red"
     else:
         lev = get_max_lev(orcid_field=orcid_field)
-        plcol = 'green'
+        plcol = "green"
 
-    bin_edges = np.arange(0,1.05,.05)
+    bin_edges = np.arange(0, 1.05, 0.05)
 
     py.clf()
 
-    n, bins, patches = py.hist(lev, bin_edges, facecolor=plcol, alpha=0.75, edgecolor='black')
+    n, bins, patches = py.hist(
+        lev, bin_edges, facecolor=plcol, alpha=0.75, edgecolor="black"
+    )
 
-    nperc = 100. * n / n.sum()
+    nperc = 100.0 * n / n.sum()
     for i in range(len(bins[0:-1])):
         if nperc[i] > 0.1:
-            py.text(bins[i], n[i] + n.max() * .01, '{:.1f}%'.format(nperc[i]))
+            py.text(bins[i], n[i] + n.max() * 0.01, "{:.1f}%".format(nperc[i]))
 
-    py.text(0.05,n.max() * 0.95,'Median: {:.3f}'.format(np.median(lev)))
-    py.text(0.05,n.max() * 0.9, 'Mean: {:.3f}'.format(np.mean(lev)))
-    py.text(0.05,n.max() * 0.85, 'Standard deviation: {:.3f}'.format(np.std(lev)))
+    py.text(0.05, n.max() * 0.95, "Median: {:.3f}".format(np.median(lev)))
+    py.text(0.05, n.max() * 0.9, "Mean: {:.3f}".format(np.mean(lev)))
+    py.text(0.05, n.max() * 0.85, "Standard deviation: {:.3f}".format(np.std(lev)))
 
-    py.axvline(x=config['MIN_LEVENSHTEIN_RATIO'],ls='--')
-    py.xlabel('Levenshtein distance ratio')
-    py.ylabel('No. of claims')
+    py.axvline(x=config["MIN_LEVENSHTEIN_RATIO"], ls="--")
+    py.xlabel("Levenshtein distance ratio")
+    py.ylabel("No. of claims")
     py.title(orcid_field)
 
-    np.save(save_path+'/maxlev_' + orcid_field + '.npy',lev)
-    py.savefig(save_path+'/maxlev_' + orcid_field + '.png')
+    np.save(save_path + "/maxlev_" + orcid_field + ".npy", lev)
+    py.savefig(save_path + "/maxlev_" + orcid_field + ".png")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # When running from command line, enter the save_path as an argument. This
     # is the path to which the plots and data will be saved.
 
@@ -176,7 +201,7 @@ if __name__ == '__main__':
     save_path = sys.argv[1]
     config = {}
     config.update(load_config())
-    plot_hist(orcid_field='orcid_user',save_path=save_path)
-    plot_hist(orcid_field='orcid_other',save_path=save_path)
-    plot_hist(orcid_field='orcid_pub',save_path=save_path)
-    plot_hist(orcid_field='mismatch',save_path=save_path)
+    plot_hist(orcid_field="orcid_user", save_path=save_path)
+    plot_hist(orcid_field="orcid_other", save_path=save_path)
+    plot_hist(orcid_field="orcid_pub", save_path=save_path)
+    plot_hist(orcid_field="mismatch", save_path=save_path)
